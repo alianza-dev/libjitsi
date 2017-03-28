9121ade41d97b7f0c83250bb3c7fe63544236e6f
Mike Saavedra <msaavedra@alianza.com>
2017-03-28 15:30:24 -0600

    added convenience methods for recording bytes and shorts to file

--- src/org/jitsi/impl/neomedia/recording/RTPRecorder.java
+++ src/org/jitsi/impl/neomedia/recording/RTPRecorder.java
@@ -1,21 +1,27 @@
 package org.jitsi.impl.neomedia.recording;
 
+import net.sf.fmj.utility.IOUtils;
 import org.jitsi.service.neomedia.RawPacket;
+import org.jitsi.util.SoundFileUtils;
 
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * Created by msaavedra on 2/28/17.
  */
 public class RTPRecorder {
     private static final ConcurrentHashMap<String, RTPRecorder> RECORDER_MAP = new ConcurrentHashMap<>();
+    private static final HashSet<String> FILE_APPEND_SET = new HashSet<>();
 
     private String recorderName;
     private TreeSet<RawPacket> packets = new TreeSet<>(new Comparator<RawPacket>() {
@@ -80,6 +86,41 @@ public class RTPRecorder {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void saveBytesToFile(byte[] bytes, String fileName) {
+        saveBytesToFile(bytes, fileName, false);
+    }
+
+    private static synchronized void saveBytesToFile(byte[] bytes, String fileName, boolean append) {
+        if (bytes != null && bytes.length > 0) {
+            try (FileOutputStream fos = new FileOutputStream(fileName, append)) {
+                fos.write(bytes);
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+    }
+
+    public static void saveShortsToFile(short[] shorts, String fileName) {
+        if (shorts != null && shorts.length > 0) {
+            byte[] bytes = new byte[shorts.length*2];
+            for (int s=0, b=0; s<shorts.length; s++, b+=2) {
+                bytes[b] = (byte) (shorts[s] >> 8);
+                bytes[b+1] = (byte) shorts[s];
+            }
+            saveBytesToFile(bytes, fileName);
+        }
+    }
+
+    public static synchronized void appendBytesToFile(byte[] bytes, int offset, int length, String fileName) {
+        byte[] bytesToWrite = Arrays.copyOfRange(bytes, offset, offset + length);
+        if (FILE_APPEND_SET.contains(fileName)) {
+            saveBytesToFile(bytesToWrite, fileName, true);
+        } else {
+            FILE_APPEND_SET.add(fileName);
+            saveBytesToFile(bytesToWrite, fileName, false);
         }
     }
 
